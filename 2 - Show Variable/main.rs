325fc63597120d27d77c354742d552eb8c6b4595
riktus <riktus2007@mail.ru>
2016-03-24 14:58:13 +0200

    Second Project

---
+++ 2 - Show Variable/main.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let number = 5;
+    let word = "string";
+
+    println!("{:?}", number);
+    println!("{:?}", word);
+
+    println!("");
+    
+    println!("{:?}", number + 10);
+}
