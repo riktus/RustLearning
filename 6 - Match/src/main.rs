40621fb436075ff58fabdf0ec37085059f43b89e
riktus <riktus2007@mail.ru>
2016-03-29 15:38:26 +0200

    New Project

---
+++ 6 - Match/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let number = 4;
+
+    match number {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        5 => println!("five"),
+        _ => println!("something bigger"),
+    }
+}
