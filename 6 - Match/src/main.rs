8ed85f67e7a2534429aabf91a101b3dbd89867df
riktus <riktus2007@mail.ru>
2016-03-31 10:25:56 +0200

    Little update

--- 6 - Match/src/main.rs
+++ 6 - Match/src/main.rs
@@ -8,5 +8,15 @@ fn main() {
         4 => println!("four"),
         5 => println!("five"),
         _ => println!("something bigger"),
+    };
+
+    println!("");
+
+    match number{
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 | 5 => println!("four or five"),
+        _ => println!("something else"),
     }
 }
